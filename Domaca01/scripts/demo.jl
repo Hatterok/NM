22c7bdea0e623bc89861bdd3f8b5cb463f813b8c
Martin Vuk <martin.vuk@agilicity.com>
2024-04-23 19:17:39 +0200

    Pripravi paket za 1. domačo nalogo

--- Domaca01/scripts/demo.jl
+++ Domaca01/scripts/demo.jl
@@ -1,40 +1,38 @@
-using Main.Domaca01
+using Domaca01
 
 # Uporaba knjižnic LinearAlgebra in Plots
 using LinearAlgebra, Plots
 
 # Primer razpršene matrike
-A = [ -2.0 1.0 1.0 0.0 0.0 0.0;
-      1.0 -2.0 1.0 0.0 0.0 0.0;
-      0.0 1.0 -2.0 1.0 0.0 0.0;
-      0.0 1.0 0.0 -3.0 1.0 0.0;
-      0.0 0.0 0.0 1.0 -2.0 1.0;
-      0.0 0.0 0.0 1.0 1.0 -2.0;
+A = [-2.0 1.0 1.0 0.0 0.0 0.0;
+    1.0 -2.0 1.0 0.0 0.0 0.0;
+    0.0 1.0 -2.0 1.0 0.0 0.0;
+    0.0 1.0 0.0 -3.0 1.0 0.0;
+    0.0 0.0 0.0 1.0 -2.0 1.0;
+    0.0 0.0 0.0 1.0 1.0 -2.0
 ]
 
 # Zapis v strukturi RazpršenaMatrika
 B = RazpršenaMatrika(
-    [   -2.0 1.0 1.0;
+    [-2.0 1.0 1.0;
         1.0 -2.0 1.0;
         1.0 -2.0 1.0;
         1.0 -3.0 1.0;
         1.0 -2.0 1.0;
-        1.0 1.0 -2.0;
-    ],
-
-    [   1 2 3;
+        1.0 1.0 -2.0
+    ], [1 2 3;
         1 2 3;
         2 3 4;
         2 4 5;
         4 5 6;
-        4 5 6;
+        4 5 6
     ]
 )
 
 # Vozlišče 1:(1,0,0)
 # Vozlišče 2:(0,1,0)
-b = [   1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0
+b = [1.0, 0.0, 0.0,
+    0.0, 1.0, 0.0
 ]
 
 # Začetni približek
@@ -50,11 +48,11 @@ w = 1.3
 LastnaVrednost = LastneVrednosti(A, w)
 
 # SOR iteracija
-xn, korak = sor(B,b,x0,w,tol)
+xn, korak = sor(B, b, x0, w, tol)
 
 # Odvisnost konvergence od relaksacijskega parametra
 OdvisnostW(B, b, x0, tol)
 
 # Preverba rezultata
-Base.product(B,xn)-b
-A*xn-b
+Base.product(B, xn) - b
+A * xn - b
