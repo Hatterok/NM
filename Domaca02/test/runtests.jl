61b0619d2175a514f46e8db97713bd7cef8e6a64
Hatterok <rok.klobucar01@gmail.com>
2024-07-18 22:09:46 +0200

    Domaca03

--- Domaca02/test/runtests.jl
+++ Domaca02/test/runtests.jl
@@ -2,50 +2,51 @@ using Test, Main.Domaca02, QuadGK
 
 @testset "Porazdelitvena funkcija normalne slucajne spremenljivke" begin
 
-    # funkcija za integracijo
+    # Funkcija za integracijo
     function f(x)
         1 / (sqrt(2π)) * exp(-(x^2) / 2)
     end
 
     f(x) = 1 / (sqrt(2π)) * exp(-(x^2) / 2)
 
-    # vhodni parametri
+    # Vhodni parametri
     a_ = -9000 # zelo slab priblizek za -neskoncno, saj funkcija quadgk vrne error za -Inf
     a = -9999999999 # Priblizek za -neskoncno
     b = 0.3
     epsilon = 1e-7
 
-    # klic nase funkcije
+    # Klic nase funkcije
     S, Nev, err = Domaca02_1(f, a, b, epsilon)
 
-    # klic vgrajene funkcije quadgk
+    # Klic vgrajene funkcije quadgk
     result, error = quadgk(f, a_, b)
 
-    #test
+    # Test
     @test isapprox(S, result, atol=1e-9)
 
-    # vhodni parametri
+    # Vhodni parametri
     b = 0.1
 
-    # klic nase funkcije
+    # Klic nase funkcije
     S, Nev, err = Domaca02_1(f, a, b, epsilon)
 
-    # klic vgrajene funkcije quadgk
+    # Klic vgrajene funkcije quadgk
     result, error = quadgk(f, a_, b)
 
-    #test
+    # Test
     @test isapprox(S, result, atol=1e-9)
 
-    # vhodni parametri
+    # Vhodni parametri
     b = 0.69
 
-    # klic nase funkcije
+    # Klic nase funkcije
     S, Nev, err = Domaca02_1(f, a, b, epsilon)
 
-    # klic vgrajene funkcije quadgk
+    # Klic vgrajene funkcije quadgk
     result, error = quadgk(f, a_, b)
 
-    #test
+    # Test
+
     @test isapprox(S, result, atol=1e-9)
 end
 
@@ -55,23 +56,23 @@ end
 
 @testset "Gauss-Legendrove kvadrature" begin
 
-    # funkcija za integracijo
+    # Funkcija za integracijo
     function integrand(x)
         sin(x) / x
     end
 
     f(x) = sin(x) / x
 
-    # vhodni parametri
+    # Vhodni parametri
     a, b = 0.0, 5.0
     zeljena_natancnost = 1e-10
 
-    # klic nase funkcije
+    # Klic nase funkcije
     priblizek, koraki = Domaca02_2(integrand, a, b, zeljena_natancnost)
 
-    # klic vgrajene funkcije quadgk
+    # Klic vgrajene funkcije quadgk
     result, error = quadgk(f, a, b)
 
-    # test
+    # Test
     @test isapprox(priblizek, result, atol=1e-11)
 end
---
+++ Domaca03/README.txt
@@ -0,0 +1,47 @@
+• ime in priimek avtorja:
+
+    	Rok Klobučar
+
+• opis naloge:
+
+    	Matematično nihalo
+	Kotni odmik θ(t) (v radianih) pri nedušenem nihanju nitnega nihala opišemo z diferencialno enačbo
+
+	g/lsin(θ(t))+θ′′(t)=0,θ(0)=θ0, θ′(0)=θ′0,
+
+	kjer je g=9.80665m/s^2 težni pospešek in l dolžina nihala. Napišite funkcijo nihalo, ki računa odmik nihala ob določenem času. Enačbo drugega reda prevedite na sistem prvega reda in računajte z metodo Runge-Kutta četrtega reda:
+
+	k1=hf(xn,yn)
+	k2=hf(xn+h/2,yn+k1/2)
+	k3=hf(xn+h/2,yn+k2/2)
+	k4=hf(xn+h,yn+k3)
+	yn+1=yn+(k1+2k2+2k3+k4)/6.
+
+	Klic funkcije naj bo oblike odmik=nihalo(l,t,theta0,dtheta0,n)
+
+	kjer je odmik enak odmiku nihala ob času t,
+	dolžina nihala je l,
+	začetni odmik (odmik ob času 0) je theta0
+	in začetna kotna hitrost (θ′(0)) je dtheta0,
+	interval [0,t] razdelimo na n podintervalov enake dolžine.
+
+	Primerjajte rešitev z nihanjem harmoničnega nihala. Za razliko od harmoničnega nihala (sinusno nihanje), je pri matematičnem nihalu nihajni čas odvisen od začetnih pogojev (energije). Narišite graf, ki predstavlja, kako se nihajni čas spreminja z energijo nihala.
+
+• navodila kako uporabiti kodo:
+
+    Za uporabo so potrebne knjižnici Plots in Test.
+
+    Ko poženete julio, najprej vnesete ukaz using Revise [1]
+    v paketnem načinu napišete (v.01) pkg>activate Domaca03
+
+    Kodo iz Domaca03\scripts\demo.jl poženete z Shift-Enter v VS Code ali z ukazom julia> include("Domaca03\scripts\demo.jl").
+    Kode iz Domaca03\src NE nalagate s Shift-Enter. Revise bi moral poskrbeti, da se bo avtomatsko naložila ob vsaki spremembi.
+
+• navodila, kako pognati teste:
+
+    Glejte prejšnjo točko.
+    Teste poženete v paketnem načinu z ukazom (Domaca03) pkg> test.
+
+• navodila, kako ustvariti poročilo:
+
+    Poročilo je že ustvarjeno v PDF obliki. :)
